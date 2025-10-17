2c7f5479b4fb6534a6c40187246b1984f4bfc4f3
李yu <614331224@qq.com>
2025-10-17 15:55:17 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: false,
+  productionSourceMap: false
+}
